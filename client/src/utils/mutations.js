13955a5ba43b1d231a5fd2a8d6e320595e25f7cc
Adam Girard-McFadden <adam_m_20@hotmail.com>
2021-10-13 14:28:47 -0400

    front-end mutations are complete - i think

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,68 @@
+// import dependencies -- apollo client
+import { gql } from "@apollo/client";
+
+// LOGIN_USER mutation
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// add a new user
+export const ADD_USER = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+// add to saved books
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: savedBook!) {
+    saveBook(input: $input) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
+
+// remove a book from saved books
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    removeBook(bookId: $bookId) {
+      _id
+      username
+      email
+      bookCount
+      savedBooks {
+        bookId
+        authors
+        title
+        description
+        image
+        link
+      }
+    }
+  }
+`;
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,5 +1,7 @@
+// import dependencies -- apollo client
 import { gql } from "@apollo/client";
 
+// export GET_ME query
 export const GET_ME = gql`
   {
     me {
